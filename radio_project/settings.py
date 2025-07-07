c4e0d676b62e9116774476a84ac0a88c9fb41f01
heisenbug0 <vegetablechickenspice@gmail.com>
2025-07-07 15:39:16 +0100

    Fix WebSocket by running in ASGI mode

--- radio_project/settings.py
+++ radio_project/settings.py
@@ -9,6 +9,7 @@ DEBUG = config('DEBUG', default=True, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
 
 INSTALLED_APPS = [
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
